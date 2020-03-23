bb70ee47a066dac8da0c461fcc680fdc4ede1cba
Jeong <inyeop0212@gmail.com>
2020-03-24 07:52:01 +0900

    Update App
    Add message handler

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,9 +5,14 @@ const socket = io.connect('http://localhost:8080');
 
 export default function App () {
   const [username, setUsername] = useState('');
+  const [message, setMessage] = useState('');
 
   socket.on('chat start', peerName => {
     console.log(peerName);
+  });
+
+  socket.on('message', message => {
+    console.log(message);
   });
 
   function usernameChangeHandler (e) {
@@ -18,6 +23,15 @@ export default function App () {
     e.preventDefault();
     socket.emit('login', username);
   }
+
+  function chatChangeHandler (e) {
+    setMessage(e.target.value);
+  }
+
+  function chatSubmitHandler (e) {
+    e.preventDefault();
+    socket.emit('message', message);
+  }
   return (
     <div>
       <form onSubmit={usernameSubmitHandler}>
@@ -25,6 +39,19 @@ export default function App () {
           <legend>Set your nickname</legend>
           <p>
             <input type="text" value={username} onChange={usernameChangeHandler} />
+          </p>
+          <p>
+            <button type="submit">Submit</button>
+          </p>
+        </fieldset>
+      </form>
+      <form onSubmit={chatSubmitHandler}>
+        <fieldset>
+          <legend>Chat Area</legend>
+          <ul>
+          </ul>
+          <p>
+            <input type="text" value={message} onChange={chatChangeHandler}/>
           </p>
           <p>
             <button type="submit">Submit</button>
