355b444db5803df370432f6458a49073ef777771
Jeong <inyeop0212@gmail.com>
2020-03-23 11:33:26 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Random chat App frontend
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDom from 'react-dom';
+import React from 'react';
+
+ReactDom.render(
+  <div>I'm App</div>,
+  document.getElementById('root')
+);
