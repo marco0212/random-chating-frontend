62f30ff27c6f8d24da3241e2b7105dcdf168f232
Jeong <inyeop0212@gmail.com>
2020-03-24 07:40:30 +0900

    Render App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import io from 'socket.io-client';
 
 const socket = io.connect('http://localhost:8080');
 
 export default function App () {
-  return <div>App</div>
+  const [username, setUsername] = useState('');
+
+  socket.on('chat start', peerName => {
+    console.log(peerName);
+  });
+
+  function usernameChangeHandler (e) {
+    setUsername(e.target.value);
+  }
+
+  function usernameSubmitHandler (e) {
+    e.preventDefault();
+    socket.emit('login', username);
+  }
+  return (
+    <div>
+      <form onSubmit={usernameSubmitHandler}>
+        <fieldset>
+          <legend>Set your nickname</legend>
+          <p>
+            <input type="text" value={username} onChange={usernameChangeHandler} />
+          </p>
+          <p>
+            <button type="submit">Submit</button>
+          </p>
+        </fieldset>
+      </form>
+    </div>
+  )
 }
