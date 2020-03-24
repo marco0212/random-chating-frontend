92e8926077c42464d2c9e7f8edae69e6f0c5944f
Jeong <inyeop0212@gmail.com>
2020-03-24 11:38:14 +0900

    Update App
    Add leave room handler

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,14 +5,32 @@ const socket = io.connect('http://localhost:8080');
 
 export default function App () {
   const [username, setUsername] = useState('');
-  const [message, setMessage] = useState('');
+  const [peerName, setPeerName] = useState('');
+  const [chatText, setChatText] = useState('');
+  const [chats, setChats] = useState([]);
 
   socket.on('chat start', peerName => {
-    console.log(peerName);
+    const message = `${peerName} was join.`;
+    const chatEle = createChatEle('log', message);
+
+    setPeerName(peerName);
+    appendChatEle(chatEle);
+  });
+
+  socket.on('typing', () => {
+    
+  });
+
+  socket.on('leave room', () => {
+    const message = `${peerName} was leave.`;
+    const chatEle = createChatEle('log', message);
+
+    appendChatEle(chatEle);
   });
 
   socket.on('message', message => {
-    console.log(message);
+    const chatEle = createChatEle('from', message);
+    appendChatEle(chatEle);
   });
 
   function usernameChangeHandler (e) {
@@ -25,15 +43,35 @@ export default function App () {
   }
 
   function chatChangeHandler (e) {
-    setMessage(e.target.value);
+    setChatText(e.target.value);
+    socket.emit('typing');
   }
 
   function chatSubmitHandler (e) {
     e.preventDefault();
-    socket.emit('message', message);
+    const chatEle = createChatEle('to', chatText);
+
+    appendChatEle(chatEle);
+    socket.emit('message', chatText);
+  }
+
+  function createChatEle(type, message) {
+    return { type, message };
+  }
+
+  function appendChatEle(ele) {
+    const copyChats = chats.slice();
+
+    copyChats.push(ele);
+    setChats(copyChats);
+  }
+
+  function next () {
+    socket.emit('leave room');
   }
   return (
     <div>
+      <button onClick={next}>next</button>
       <form onSubmit={usernameSubmitHandler}>
         <fieldset>
           <legend>Set your nickname</legend>
@@ -49,9 +87,17 @@ export default function App () {
         <fieldset>
           <legend>Chat Area</legend>
           <ul>
+            {
+              chats.map((chat, index) => {
+                const { type, message } = chat;
+                return (
+                  <li key={`chat-${index}`}>{ message }</li>
+                )
+              })
+            }
           </ul>
           <p>
-            <input type="text" value={message} onChange={chatChangeHandler}/>
+            <input type="text" value={chatText} onChange={chatChangeHandler}/>
           </p>
           <p>
             <button type="submit">Submit</button>
