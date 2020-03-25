e659f485efd699189dc2ab9c0585c12966cc40ee
Jeong <inyeop0212@gmail.com>
2020-03-25 14:01:20 +0900

    Test Message component

---
+++ src/components/Message/Message.spec.js
@@ -0,0 +1,13 @@
+import { render, fireEvent } from '@testing-library/react';
+import React from 'react';
+import Message from './Message';
+
+describe('<Message />', () => {
+  const someString = 'Hello World?';
+
+  it('should includes text got by prop text', () => {
+    const { getByText } = render(<Message text={someString}/>);
+
+    expect(getByText(someString)).toBeTruthy();
+  });
+});
